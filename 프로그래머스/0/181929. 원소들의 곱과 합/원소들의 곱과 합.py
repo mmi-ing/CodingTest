6df0da7704206c9668c38416a46be8dfbc5a94a6
mmi-ing <77337846+mmi-ing@users.noreply.github.com>
2025-06-07 00:54:38 +0900

    원소들의 곱과 합.py

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,13 +1,21 @@
+# def solution(num_list):
+#     answer = 0
+#     num1 = 1
+#     num2 = 0
+#     num3 = 0
+#     for i in num_list:
+#         num1 = num1 * i
+#         num2 = num2 + i
+#         num3 = num2**2
+#     if num1 < num3:
+#         answer = 1
+#     else: answer = 0
+#     return answer
 def solution(num_list):
-    answer = 0
-    num1 = 1
-    num2 = 0
-    num3 = 0
+    num1 = 1  # 곱
+    num2 = 0  # 합
     for i in num_list:
-        num1 = num1 * i
-        num2 = num2 + i
-        num3 = num2**2
-    if num1 < num3:
-        answer = 1
-    else: answer = 0
-    return answer
+        num1 *= i
+        num2 += i
+    num3 = num2 ** 2  # 합의 제곱
+    return 1 if num1 < num3 else 0
