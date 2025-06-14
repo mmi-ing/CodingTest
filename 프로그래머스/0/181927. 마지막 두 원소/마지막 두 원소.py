e9e8710d7bf85bc328c9463dbe764b2b10666ab2
mmi-ing <77337846+mmi-ing@users.noreply.github.com>
2025-06-14 12:48:40 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,7 @@
+def solution(num_list):
+    num1 = num_list[-1]
+    num2 = num_list[-2]
+    if num1 > num2 :
+        num_list.append(num1-num2)
+    else: num_list.append(num1*2)
+    return num_list
