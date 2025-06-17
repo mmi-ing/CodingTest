65e11e5ab59f976ed8604f5ed9ec71a894181c9a
mmi-ing <77337846+mmi-ing@users.noreply.github.com>
2025-06-17 18:00:40 +0900

    [level 0] Title: 코드 처리하기, Time: 12.61 ms, Memory: 9.45 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,14 @@
+def solution(code):
+    mode = 0
+    ret = ''
+    
+    for idx, val in enumerate(code):
+        if val == '1':
+            mode = 1 - mode
+        else:
+            if mode == 0 and idx % 2 == 0:
+                ret += val
+            elif mode == 1 and idx % 2 != 0:
+                ret += val
+        
+    return ret if ret else "EMPTY"
