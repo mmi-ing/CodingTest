dc4429efa62cdd5bb2250dbb7ad374170400aacd
mmi-ing <77337846+mmi-ing@users.noreply.github.com>
2025-06-26 15:54:15 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int numer = numer1 * denom2 + numer2 * denom1;
+        int denom = denom1 * denom2;
+        int gcd = getGCD(numer, denom);
+        return new int[]{numer / gcd, denom/gcd};
+    }
+    
+    private int getGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
